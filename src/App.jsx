eedf30afbd55cd896fe5a128009cfa89235c091d
Aiden Redmond <adnrdmnd@gmail.com>
2025-06-17 12:18:30 -0400

    actually bare bones

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
-import { useState } from "react";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div></div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>hello world</h1>
     </>
   );
 }
